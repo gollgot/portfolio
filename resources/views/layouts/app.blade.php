97b0f03f549424d7d2cb71c1ca6ceb7f53543f42
Loïc Dessaules <loic.dessaules@gmail.com>
2021-02-08 19:25:23 +0100

    Config project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Portfolio</title>
+
+
+        </style>
+    </head>
+    <body>
+        <h1>Portfolio</h1>
+    </body>
+</html>
