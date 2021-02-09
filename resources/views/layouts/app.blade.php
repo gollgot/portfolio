0e6a31f19413b6d85629e66cea352db6ff279d78
Loïc Dessaules <loic.dessaules@gmail.com>
2021-02-09 11:09:21 +0100

    Add hero section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,14 +13,16 @@
     <body>
 
         <!-- Languages -->
-        <div class="float-right mt-4 mr-4 font-light">
+        <div class="text-right mt-4 mr-4 font-light">
             <a class="{{ App::getLocale() != 'fr' ? 'font-bold' : '' }}" href="{{ route(Route::currentRouteName(), 'en') }}">EN</a> | <a class="{{ App::getLocale() == 'fr' ? 'font-bold' : '' }}" href="{{ route(Route::currentRouteName(), 'fr') }}">FR</a>
         </div>
 
         <!-- Hero Section -->
-        <section class="text-center">
-            <h1 class="text-4xl font-bold">{{ __('pages.home.heroSection.title') }}</h1>
-            <h2 class="text-lg font-thin">{{ __('pages.home.heroSection.subtitle') }}</h2>
+        <section class="text-center p-5">
+            <h1 class="text-4xl font-bold mt-20">{{ __('pages.home.heroSection.title') }}</h1>
+            <h2 class="text-lg font-thin mt-4">{{ __('pages.home.heroSection.subtitle') }}</h2>
+            <img class="mt-10 h-56 m-auto" src="{{ asset('images/avatar.png') }}" alt="Avatar">
+            <img class="mt-24 m-auto" src="{{ asset('images/hero.svg') }}" alt="Hero-image">
         </section>
 
     </body>
